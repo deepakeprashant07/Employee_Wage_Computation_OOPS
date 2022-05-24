381b0f85c1253ec637681e6b855a4804b1ded945
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-24 23:50:12 +0530

    ADDED: Employee Attendence Check

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,10 +1,23 @@
 public class EmployeeWage {
+    public static final int PRESENT = 1;
+
     public void display() {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
     }
 
+    public void attendence(int x) {
+        if (PRESENT == x) {
+            System.out.println("EMPLOYEE PRESENT");
+        } else {
+            System.out.println("EMPLOYEE ABSENT");
+        }
+
+    }
+
     public static void main(String[] args) {
         EmployeeWage EW = new EmployeeWage();
+        double randomNumber = Math.floor(Math.random() * 10) % 2;
         EW.display();
+        EW.attendence((int) randomNumber);
     }
 }
