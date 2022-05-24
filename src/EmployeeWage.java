7447e7ced65f6aac86f3771c95cad77781aaed79
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-24 23:28:52 +0530

    ADDED: Welcome To Employee Wage Computation Message

---
+++ src/EmployeeWage.java
@@ -0,0 +1,10 @@
+public class EmployeeWage {
+    public void display() {
+        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+    }
+
+    public static void main(String[] args) {
+        EmployeeWage EW = new EmployeeWage();
+        EW.display();
+    }
+}
