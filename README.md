00c78711933f0b294727f1aeb3940c83af87fb09
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-25 17:09:11 +0530

    ADDED: Employee Daily Wage Calculate

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+
 # Employee_Wage_Computation_OOPS
--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,6 @@
 public class EmployeeWage {
     public static final int PRESENT = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
     public void display() {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
@@ -8,10 +9,17 @@ public class EmployeeWage {
     public void attendence(int x) {
         if (PRESENT == x) {
             System.out.println("EMPLOYEE PRESENT");
+            dailyWageCalculate();
         } else {
             System.out.println("EMPLOYEE ABSENT");
         }
 
+    }
+
+    public void dailyWageCalculate() {
+        int fullHour = 8;
+        int dailyWage = fullHour * EMP_RATE_PER_HOUR;
+        System.out.println("EMPLOYEE DAILY WAGE = " + dailyWage);
     }
 
     public static void main(String[] args) {
