102e43b27dda99185f694934d049713aa9fe864b
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-25 22:22:15 +0530

    ADDED: Employee Part Time Wage Calculate

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,6 +1,8 @@
 public class EmployeeWage {
     public static final int PRESENT = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
     public void display() {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
@@ -17,9 +19,21 @@ public class EmployeeWage {
     }
 
     public void dailyWageCalculate() {
-        int fullHour = 8;
-        int dailyWage = fullHour * EMP_RATE_PER_HOUR;
-        System.out.println("EMPLOYEE DAILY WAGE = " + dailyWage);
+        int time = partTimeCheck();
+        int dailyWage = time * EMP_RATE_PER_HOUR;
+        System.out.println("EMPLOYEE " + time + " HOURS WAGE = " + dailyWage);
+    }
+
+    public int partTimeCheck() {
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if ((int) empcheck == IS_PART_TIME) {
+            int partTime = 8;
+            return partTime;
+        } else if ((int) empcheck == IS_FULL_TIME) {
+            int fullTime = 12;
+            return fullTime;
+        }
+        return 8;
     }
 
     public static void main(String[] args) {
